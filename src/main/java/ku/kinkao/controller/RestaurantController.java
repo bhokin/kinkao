2c39ac9abd1f6023a488fc832d9f97c2b9f6a81b
Bhokin Watanapitak <75792557+bhokin@users.noreply.github.com>
2023-02-22 11:25:50 +0700

    Add a restaurant form

--- src/main/java/ku/kinkao/controller/RestaurantController.java
+++ src/main/java/ku/kinkao/controller/RestaurantController.java
@@ -1,10 +1,13 @@
 package ku.kinkao.controller;
 
+import ku.kinkao.model.Restaurant;
 import ku.kinkao.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -19,5 +22,18 @@ public class RestaurantController {
        model.addAttribute("restaurants", repository.findAll());
        return "restaurant";  // return name of homepage which is restaurant.html
    }
+
+   @GetMapping("/restaurant/add")
+   public String getAddPage() {
+       return "restaurant-add";  // return restaurant-add.html
+   }
+
+   @PostMapping("/restaurant/add")
+   public String addRestaurant(@ModelAttribute Restaurant restaurant, 
+                               Model model) {
+       repository.save(restaurant);
+       return "redirect:/restaurant";
+   }
+
 }
 
