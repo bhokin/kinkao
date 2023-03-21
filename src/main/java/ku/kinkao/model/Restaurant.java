d57730fb012e3865bdb2aa582da7449835ca398c
Bhokin Watanapitak <75792557+bhokin@users.noreply.github.com>
2023-03-21 17:49:23 +0700

    Delete all getter and setter

--- src/main/java/ku/kinkao/model/Restaurant.java
+++ src/main/java/ku/kinkao/model/Restaurant.java
@@ -19,37 +19,5 @@ public class Restaurant {
    private String name;
    private String address;
    private int rating;
-
-   public int getId() {
-       return id;
-   }
-
-   public void setId(int id) {
-       this.id = id;
-   }
-
-   public String getName() {
-       return name;
-   }
-
-   public void setName(String name) {
-       this.name = name;
-   }
-
-   public String getAddress() {
-       return address;
-   }
-
-   public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
 }
 
