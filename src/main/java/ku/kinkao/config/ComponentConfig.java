3178e1db85eb5fe25632590e6a5f407356efd9aa
Bhokin Watanapitak <75792557+bhokin@users.noreply.github.com>
2023-03-29 10:05:20 +0700

    Request an Access Token

--- src/main/java/ku/kinkao/config/ComponentConfig.java
+++ src/main/java/ku/kinkao/config/ComponentConfig.java
@@ -1,8 +1,10 @@
 package ku.kinkao.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ComponentConfig {
@@ -11,4 +13,10 @@ public class ComponentConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
 }
