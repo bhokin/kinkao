8821de80653eb53649cafbf4548e6d2934fad60c
Bhokin Watanapitak <75792557+bhokin@users.noreply.github.com>
2023-03-30 02:02:01 +0700

    Connect to API Service using JWT Token

--- src/main/java/ku/kinkao/service/RestaurantService.java
+++ src/main/java/ku/kinkao/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package ku.kinkao.service;
 
-import ku.kinkao.dto.RestaurantDto;
+import ku.kinkao.dto.RestaurantRequest;
+import ku.kinkao.dto.RestaurantResponse;
 import ku.kinkao.model.Restaurant;
 import ku.kinkao.repository.RestaurantRepository;
 import org.modelmapper.ModelMapper;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Service
 public class RestaurantService {
@@ -19,20 +21,29 @@ public class RestaurantService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public List<RestaurantDto> getRestaurants() {
-        List<Restaurant> restaurants = repository.findAll();
+    public RestaurantResponse getRestaurantById(UUID restaurantId) {
+        Restaurant restaurant = repository.findById(restaurantId).get();
+        return modelMapper.map(restaurant, RestaurantResponse.class);
+    }
 
-        List<RestaurantDto> dtos = restaurants
+
+    // mapping DAO -> DTO
+    public List<RestaurantResponse> getRestaurants() {
+        List<Restaurant> restaurants = repository.findAll();
+        System.out.println(restaurants);
+
+        List<RestaurantResponse> dtos = restaurants
                 .stream()
                 .map(restaurant -> modelMapper.map(restaurant,
-                        RestaurantDto.class))
+                        RestaurantResponse.class))
                 .collect(Collectors.toList());
 
         return dtos;
     }
 
-    public void create(RestaurantDto restaurantDto) {
-        Restaurant restaurant = modelMapper.map(restaurantDto,
+    // mapping DTO -> DAO
+    public void create(RestaurantRequest restaurantRequest) {
+        Restaurant restaurant = modelMapper.map(restaurantRequest,
                 Restaurant.class);
         restaurant.setCreatedAt(Instant.now());
         repository.save(restaurant);
